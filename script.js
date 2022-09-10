9d36ca1da2683b515df4511545c59f1b3a7fe251
Rammigas <Romanvashkevich1995@mail.ru>
2022-09-10 21:10:21 +0300

    feat: add scroll scripts

--- script.js
+++ script.js
@@ -8,4 +8,25 @@ function activeButton() {
 	headerMenu.classList.toggle('active')
 	body.classList.toggle('lock')
 }
+// scroll
+const menuLinks = document.querySelectorAll('.header__link')
+let arrLinks = Array.from(menuLinks)
 
+arrLinks.forEach(menuLink => {
+	menuLink.addEventListener('click', onMenuLinkClick)
+})
+function onMenuLinkClick(e) {
+	const menuLink = e.target;
+	if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+		const gotoBlock = document.querySelector(menuLink.dataset.goto)
+		const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+		window.scrollTo({
+			top: gotoBlockValue,
+			behavior: "smooth"
+		});
+		e.preventDefault();
+	}
+	burgerButton.classList.remove('active')
+	headerMenu.classList.remove('active')
+	body.classList.remove('lock')
+}
